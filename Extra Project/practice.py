ca6350f0ef7f8f004043ad69948fad35a2417d6f
12862 <daniel.gosses@vtsd.com>
2024-05-09 08:23:44 -0400

    23-24 Exam Prep

---
+++ Extra Project/practice.py
@@ -0,0 +1,19 @@
+def swap1(item1 , item2):
+    item1 = item2
+    item2 = item1
+
+def swap2(item1, item2):
+    temp = item1
+    item1 = item2
+    item2 = item1
+
+
+
+
+
+'''
+A. Algorithm 1 works
+B. Algorithm 2 works
+C. They both work
+D. Neither Work
+'''
