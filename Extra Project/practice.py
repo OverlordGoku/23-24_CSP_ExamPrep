25601fad4769c5bd9372e28e312e68b6f71c31d4
12862 <daniel.gosses@vtsd.com>
2024-05-10 07:57:03 -0400

    23-24 Exam Prep

--- Extra Project/practice.py
+++ Extra Project/practice.py
@@ -8,12 +8,41 @@ def swap2(item1, item2):
     item2 = item1
 
 
-
-
-
 '''
 A. Algorithm 1 works
 B. Algorithm 2 works
 C. They both work
 D. Neither Work
 '''
+
+#what line would you replace with item2 = temp?
+# A.Line 2
+# B.Line 7
+# C.Line 8
+# D.Line 4
+
+# Method returns the highest value
+def sortValue(val1,val2,val3):
+    if(val1>val2):
+        return val
+    if(val2 > val3):
+     return val2
+'''
+What set of values would not return the correct answer? Select 2
+A. 5,4,3
+B. 2,1,5
+C. 2,1,4
+D. 2,5,3
+'''
+
+j = 1
+k = 0
+
+while j < 30:
+    k = 2 * j
+    j *= 3
+    print(j)
+
+
+
+
